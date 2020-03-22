1e41247dc0dca76d1437ea36a48fcb68674d9c83
alonecheer <alonecheer@hotmail.com>
2020-03-22 15:08:18 +0700

    chang to class component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
+import React,{Component} from 'react'
 
-
-const Home = () => (
-  <div>
-    sdfdsf
+class Index extends Component {
+  constructor()
+  {
+    super();
+    this.state={
+      email:null,
+      password:null,
+    }
+  }
+  render(){
+    return(
+<div>
+    <h1>Jwt Token with</h1>
+    <div>
+      <input type="text" onChange={(event)=>{this.setState({email:event.target.value})}}></input><br></br><br></br>
+      <input type="text" onChange={(event)=>{this.setState({password:event.target.value})}}></input><br></br><br></br>
+      <button onClick={()=>{this.login()}}>Login</button>
+    </div>
   </div>
-)
+    )
+  }
+}
 
-export default Home
+export default Index
