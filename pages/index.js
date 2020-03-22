f8b9b0d573743d5bd1f72606876ae908c2cbe62e
alonecheer <alonecheer@hotmail.com>
2020-03-22 15:47:54 +0700

    change to funtion component

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,12 @@
-import React,{Component} from 'react'
 
-class Index extends Component {
-  constructor()
-  {
-    super();
-    this.state={
-      email:null,
-      password:null,
-    }
-  }
-  render(){
-    return(
-<div>
-    <h1>Jwt Token with</h1>
+import axios from "axios";
+
+const Index = () =>{
+  
+  return(
     <div>
-      <input type="text" onChange={(event)=>{this.setState({email:event.target.value})}}></input><br></br><br></br>
-      <input type="text" onChange={(event)=>{this.setState({password:event.target.value})}}></input><br></br><br></br>
-      <button onClick={()=>{this.login()}}>Login</button>
+      sdfdsf
     </div>
-  </div>
-    )
-  }
+  )
 }
-
-export default Index
+export default Index;
