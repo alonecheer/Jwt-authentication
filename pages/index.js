5450239d2750e10b8b14306f5922e2d2eb0af50e
alonecheer <alonecheer@hotmail.com>
2020-03-22 23:23:30 +0700

    store local

--- pages/index.js
+++ pages/index.js
@@ -4,40 +4,43 @@ import axios from "axios";
 const Index = () =>{
   const [username,setUsername] = useState('');
   const [password,setPassword] = useState('');
-  let [tokens,setTokens] = useState('');
-  let [status,setStatus] = useState(false);
+  const [userid,setUserid] = useState('');
+  let [store,setStore] = useState('');
+  let [login,setLogin] = useState(false);
+  
+  
   
   useEffect( () => {
-    
+   
   },[])
-  const login = async ()   => {
+  const logins = async ()   => {
     const result = await  axios.post('http://localhost:3001/auth/login',
       {username:username,password:password})
       //console.log(result.data.access_token)
-
       localStorage.setItem('login', JSON.stringify({
         token: result.data.access_token,
-        status: true  
+        login: true,  
+      
       })
       )
-      setStatus({status:true})
   }
 
+  const post = async () => {
+    let token = "Bearer " 
+
+  }
   return(
     <div>
-      {!status?
+      {!login?
       <div>
        <h1>Jwt Token with</h1>
       <input type="text" onChange={(e) => setUsername(e.target.value)} /><br></br><br></br>
       <input type="text" onChange={(e) => setPassword(e.target.value)} /><br></br><br></br>
-      <button onClick={()=>{login()}}>Login</button> 
+      <button onClick={()=>{logins()}}>Login</button> 
       </div>
       :
       <div>
-        <textarea>
-        
-        </textarea>
-        <button>submit</button>
+        <button onClick={()=>{post()}}>Post</button> 
       </div>
       }
     </div>
