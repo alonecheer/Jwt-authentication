1e112f11169f8950b14f5707a47cd51685806e2f
alonecheer <alonecheer@hotmail.com>
2020-03-22 15:59:34 +0700

    add async

--- pages/index.js
+++ pages/index.js
@@ -6,17 +6,17 @@ const Index = () =>{
   const [password,setPassword] = useState('');
   const [token,setToken] = useState('');
 
-  const login = () => {
-    const result = axios.post('http://localhost:3001/auth/login',
+  const login = async ()   => {
+    const result = await  axios.post('http://localhost:3001/auth/login',
       {username:username,password:password})
-      console.log(result.data)
+      console.log(result.data.access_token)
   }
   return(
     <div>
       <h1>Jwt Token with</h1>
       <input type="text" onChange={(e) => setUsername(e.target.value)} /><br></br><br></br>
       <input type="text" onChange={(e) => setPassword(e.target.value)} /><br></br><br></br>
-      <button onClick={()=>{this.login()}}>Login</button>
+      <button onClick={()=>{login()}}>Login</button>
     </div>
   )
 }
