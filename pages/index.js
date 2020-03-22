bfae05dc93f5e234262584465340261abfa564e1
alonecheer <alonecheer@hotmail.com>
2020-03-22 15:56:15 +0700

    add const , button

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
-
+import { useState } from "react";
 import axios from "axios";
 
 const Index = () =>{
-  
+  const [username,setUsername] = useState('');
+  const [password,setPassword] = useState('');
+  const [token,setToken] = useState('');
+
+  const login = () => {
+    const result = axios.post('http://localhost:3001/auth/login',
+      {username:username,password:password})
+      console.log(result.data)
+  }
   return(
     <div>
-      sdfdsf
+      <h1>Jwt Token with</h1>
+      <input type="text" onChange={(e) => setUsername(e.target.value)} /><br></br><br></br>
+      <input type="text" onChange={(e) => setPassword(e.target.value)} /><br></br><br></br>
+      <button onClick={()=>{this.login()}}>Login</button>
     </div>
   )
 }
